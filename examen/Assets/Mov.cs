fb2586e300577d4bcf5e269fef909103423f6090
rugybg <rugybg@gmail.com>
2018-12-12 20:42:37 +0100

    Initial commit

---
+++ examen/Assets/Mov.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov : MonoBehaviour
+{
+
+
+    //script en la bola
+    public GameObject cubo;
+
+
+    private void OnTriggerEnter3D(Collider collision)
+    {
+        Destroy(cubo);
+    }
+}
